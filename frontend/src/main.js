0efcf3a667e8cd9a2513a0217a8d858dfa4829ab
Starkin28 <victoreq00@gmail.com>
2021-04-12 11:35:07 +0200

    installed axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,21 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// BOOTSTRAP
 import BootstrapVue from 'bootstrap-vue';
 Vue.use(BootstrapVue);
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// AXIOS
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+Vue.use(VueAxios, axios);
+
+// BACKEND
+axios.defaults.baseURL = 'http://localhost:4000/api';
 
 Vue.config.productionTip = false;
 
